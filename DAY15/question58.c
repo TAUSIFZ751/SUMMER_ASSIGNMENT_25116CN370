cad4b8304b08ee1d782d0dd53eea6b0bbef8ed98
TAUSIF <mds465790@gmail.com>
2026-06-15 21:08:45 +0530

    DAY15 completed

---
+++ DAY15/question58.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,temp;
+
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    int arr[n];
+
+    printf("Enter elements: ");
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    temp=arr[0];
+
+    for(i=0;i<n-1;i++)
+        arr[i]=arr[i+1];
+
+    arr[n-1]=temp;
+
+    printf("After left rotation: ");
+    for(i=0;i<n;i++)
+        printf("%d ",arr[i]);
+
+    return 0;
+}
